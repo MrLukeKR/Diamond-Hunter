31f1fa386a01d37ddc791b26fb93c6e850a1f88f
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2016-11-18 18:33:07 +0000

    Implemented directory RegEx, started experimenting with tile viewing

--- DiamondHunter/srcMapViewer/application/MainController.java
+++ DiamondHunter/srcMapViewer/application/MainController.java
@@ -3,6 +3,8 @@ package application;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -13,6 +15,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 
 public class MainController implements Initializable {
 
@@ -20,8 +23,7 @@ public class MainController implements Initializable {
 	@FXML private Button loadMapButton;
 	@FXML private TextField tilesetDirectory;
 	@FXML private TextField mapDirectory;
-	@FXML private ImageView tilesetViewer;
-	private Image tilesetImage;
+	@FXML private HBox tilesetLibrary;
 	
 	@FXML
 	private void handleLoadingTiles(ActionEvent event){
@@ -29,9 +31,17 @@ public class MainController implements Initializable {
 
 		if(validateDirectory(directory)){
 			File imageFile = new File(directory);
-			
-			tilesetImage = new Image(imageFile.toURI().toString());
-			tilesetViewer.setImage(tilesetImage);
+			Image tilesetImage = new Image(imageFile.toURI().toString());
+
+			ImageView viewer;
+			for(int i = 0; i < 10; i ++){
+				viewer = new ImageView();
+				viewer.setImage(tilesetImage);
+				viewer.setPreserveRatio(false);
+				viewer.setFitHeight(60);
+				viewer.setFitWidth(60);
+				tilesetLibrary.getChildren().add(viewer);
+			}
 		} else {
 			System.out.println("Invalid directory entered for Map Directory");
 		//TODO: JavaFX warning notification
@@ -53,9 +63,7 @@ public class MainController implements Initializable {
 	
 	
 	private boolean validateDirectory(String directory){
-		//TODO: REGEX of directory structure
-		
-		return true;
+		return Pattern.matches("^(.+)/([^/]+)$", directory);
 	}
 	
 	private void checkInput(TextField fieldToCheck, Button buttonToAlter){
@@ -71,7 +79,6 @@ public class MainController implements Initializable {
 		assert loadMapButton != null : "Load Map button was not injected!";
 		assert tilesetDirectory != null : "Tileset Directory field was not injected!";
 		assert mapDirectory != null : "Map Directory field was not injected!";
-		assert tilesetViewer != null : "Tilset Image Viewer was not injected!";
 		
 		tilesetDirectory.textProperty().addListener(new ChangeListener<String>(){
 
