b402504773fa3250d115c1ed4ad6faaff87dcad7
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2016-12-02 13:39:28 +0000

    Fixed error that allowed TileSet viewer TileButtons to set co-ord labels

--- DiamondHunter/srcMapViewer/application/TileButton.java
+++ DiamondHunter/srcMapViewer/application/TileButton.java
@@ -19,6 +19,7 @@ public class TileButton extends ImageView{
 	public TileButton(Image icon) {
 		setImage(icon);
 		setHandlers();
+		xLoc = yLoc = -1;
 	}
 	
 	private void setHandlers(){
@@ -28,9 +29,11 @@ public class TileButton extends ImageView{
 			public void handle(MouseEvent arg0) {
 				// TODO Auto-generated method stub
 				setEffect(selected);
-				model.updateCoordinates(xLoc, yLoc);
-				controller.updateCoordinates();
-				controller.updateIsBlocked(isBlocked);
+				if(xLoc != -1 && yLoc != -1){
+					model.updateCoordinates(xLoc, yLoc);
+					controller.updateCoordinates();
+					controller.updateIsBlocked(isBlocked);
+				}
 			}
 			
 		});
