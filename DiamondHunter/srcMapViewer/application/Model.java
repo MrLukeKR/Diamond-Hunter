bb7eba4cfb0278b3318925245912e8df9f39e927
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2016-12-08 13:22:50 +0000

    Allowed writing item locations to a custom directory & file

--- DiamondHunter/srcMapViewer/application/Model.java
+++ DiamondHunter/srcMapViewer/application/Model.java
@@ -4,8 +4,11 @@ import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 import javax.imageio.ImageIO;
 
@@ -34,8 +37,19 @@ public class Model {
 	private int item =-1;
 	
 	public static int AXE = 1, BOAT = 0;
+	
 
-
+	public void saveItemMap(String dir){
+	try {
+		PrintWriter itemMapFile = new PrintWriter(dir,"UTF-8");
+		itemMapFile.println(AXE + "," + axeX + "," + axeY);
+		itemMapFile.println(BOAT + "," + boatX + "," + boatY);
+		itemMapFile.close();
+	} catch (FileNotFoundException | UnsupportedEncodingException e) {
+		e.printStackTrace();
+	}	
+	}
+	 
 public void loadTiles(String s) {
 		
 		try {
