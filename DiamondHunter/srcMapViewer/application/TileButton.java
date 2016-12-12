d558e4296ef83204827ed52c96709a12844e40b3
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2016-12-12 19:54:29 +0000

    Refactor: Set commonly used values to static final Model references

--- DiamondHunter/srcMapViewer/application/TileButton.java
+++ DiamondHunter/srcMapViewer/application/TileButton.java
@@ -13,7 +13,7 @@ public class TileButton extends ImageView{
 	private Effect selected;
 
 	private Model model;
-	private int xLoc = -1, yLoc = -1;
+	private int xLoc = Model.EMPTY, yLoc = Model.EMPTY;
 	private boolean isBlocked;
 	
 	public TileButton(Image icon) {
@@ -21,49 +21,37 @@ public class TileButton extends ImageView{
 		setHandlers();
 	}
 	
+	public void setModel (Model model){ this.model = model; }
+	public void setCoordinates(int x, int y){ xLoc = x; yLoc = y; }
+	
 	private void setHandlers(){
 		setOnMouseEntered(new EventHandler<MouseEvent>(){
 			@Override
 			public void handle(MouseEvent arg0) {
 				setEffect(selected);
-				if(xLoc != -1 && yLoc != -1)
+				if(xLoc != Model.EMPTY && yLoc != Model.EMPTY)
 					model.updateCoordinates(xLoc, yLoc, isBlocked);
 			}
-			
 		});
 		
 		setOnMouseClicked(new EventHandler<MouseEvent>(){
-
 			@Override
 			public void handle(MouseEvent arg0) {
 				model.placeItem(xLoc, yLoc, isBlocked);
 				model.updateCoordinates(xLoc, yLoc, isBlocked);
 			}
-			
 		});
 		
 		setOnMouseExited(new EventHandler<MouseEvent>(){
 			@Override
-			public void handle(MouseEvent event) {
-					setEffect(null);  
-				}
+			public void handle(MouseEvent event) { setEffect(null); }
 			});
 	}
 
-	public void setModel (Model model){
-		this.model = model;
-	}
-	
 	public void setIsBlocked(boolean blocked){
 		isBlocked = blocked;
-		if(blocked)
-			selected = new InnerShadow(5, Color.RED);
-		else
-			selected = new InnerShadow(5, Color.BLACK);
-	}
-	
-	public void setCoordinates(int x, int y){
-		xLoc = x;
-		yLoc = y;
+		
+		if(blocked) selected = new InnerShadow(5, Color.RED);
+		else 		selected = new InnerShadow(5, Color.BLACK);
 	}
 }
