6587e981223a102419676f094a360af7c35da39a
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2016-12-12 19:01:46 +0000

    Refactor: Moved any interaction with the controller to the model

--- DiamondHunter/srcMapViewer/application/TileButton.java
+++ DiamondHunter/srcMapViewer/application/TileButton.java
@@ -12,34 +12,22 @@ public class TileButton extends ImageView{
 
 	private Effect selected;
 
-	private MainController controller;
 	private Model model;
-	private int xLoc, yLoc;
+	private int xLoc = -1, yLoc = -1;
 	private boolean isBlocked;
 	
 	public TileButton(Image icon) {
 		setImage(icon);
 		setHandlers();
-		
-		xLoc = yLoc = -1;
 	}
 	
 	private void setHandlers(){
 		setOnMouseEntered(new EventHandler<MouseEvent>(){
-
 			@Override
 			public void handle(MouseEvent arg0) {
-				// TODO Auto-generated method stub
 				setEffect(selected);
-				if(xLoc != -1 && yLoc != -1){
-					model.updateCoordinates(xLoc, yLoc);
-					controller.updateCoordinates();
-					controller.updateIsBlocked(isBlocked);
-					int tempItem = model.getItem(xLoc, yLoc);
-					
-					if(tempItem != -1)
-						controller.updateHasItem(tempItem);
-				}
+				if(xLoc != -1 && yLoc != -1)
+					model.updateCoordinates(xLoc, yLoc, isBlocked);
 			}
 			
 		});
@@ -48,14 +36,12 @@ public class TileButton extends ImageView{
 
 			@Override
 			public void handle(MouseEvent arg0) {
-				if(model.placeItem(xLoc, yLoc, isBlocked))
-					controller.displayItem(xLoc, yLoc);
+				model.placeItem(xLoc, yLoc, isBlocked);
 			}
 			
 		});
 		
 		setOnMouseExited(new EventHandler<MouseEvent>(){
-
 			@Override
 			public void handle(MouseEvent event) {
 					setEffect(null);  
@@ -63,10 +49,6 @@ public class TileButton extends ImageView{
 			});
 	}
 
-	public void setController (MainController mainController){
-		controller = mainController;
-	}
-	
 	public void setModel (Model model){
 		this.model = model;
 	}
